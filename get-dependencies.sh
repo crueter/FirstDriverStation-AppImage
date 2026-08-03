40e6c4dde1beec6281ddffb51dcda0cacf93dda8
crueter <crueter@eden-emu.dev>
2026-08-03 00:27:58 -0400

    add gh workflow
    Signed-off-by: crueter <crueter@eden-emu.dev>

---
+++ get-dependencies.sh
@@ -0,0 +1,14 @@
+#!/bin/sh -e
+
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+echo "Installing build dependencies..."
+echo "---------------------------------------------------------------"
+pacman -Syu --noconfirm --overwrite "*" \
+	base-devel patchelf curl jq strace xorg-server-xvfb zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES"
+chmod +x get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-mesa libxml2-mini opus-nano
